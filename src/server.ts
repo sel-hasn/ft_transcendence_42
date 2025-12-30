b85bef350dcd7bcd973a1e58134912492c1a5169
sel-hasn <fangyuansamach@gmail.com>
2025-12-30 20:17:12 +0100

    fix: using the added functions in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,24 @@
 //server.ts
 import { config } from './config';
 import app from './app';
+import { closeDb, initDatabase } from './core/database';
+
+// Initialize the database before starting the server
+initDatabase();
 
 const server = app.listen(config.port, () => {
     console.log(`🚀 Server running on http://localhost:${config.port}`);
     console.log(`📁 Environment: ${config.nodeEnv}`);
 });
 
-// const gracefulShutdown = (signal: string) => {
-//     console.log(`${signal} signal received: closing HTTP server`);
+const gracefulShutdown = (signal: string) => {
+    console.log(`${signal} signal received: Closing server...`);
 
-//     server.close(() => {
-//         console.log('HTTP server closed');
+    server.close(() => {
+        closeDb();
+        process.exit(0);
+    });
+};
 
-//         db.close((err) => {
-//             if (err) {
-//                 console.error('Error closing SQLite connection', err);
-//                 process.exit(1);
-//             }
-
-//             console.log('SQLite connection closed');
-//             process.exit(0);
-//         });
-//     });
-// };
-
-// process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
-// process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
