6fc8edca4914f2b2863765cea473e1fb3f07cede
sel-hasn <fangyuansamach@gmail.com>
2026-01-02 23:33:26 +0100

    adding utils

--- src/utils/crypt.ts
+++ src/utils/crypt.ts
@@ -11,3 +11,12 @@ export async function verifyPassword(password: string, hash: string): Promise<bo
     return bcrypt.compare(password, hash);
 }
 
+export function hashToken(token: string): string {
+  return crypto.createHash('sha256').update(token).digest('hex');
+}
+
+export function getExpirationDate(days: number): Date {
+  const date = new Date();
+  date.setDate(date.getDate() + days);
+  return date;
+}
---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config';
+
+export const signJwt = (
+    object: Object,
+    options?: jwt.SignOptions | undefined
+) => {
+    return jwt.sign(object, config.jwtSecret, {
+        ...(options && options),
+    });
+};
+
+export const verifyJwt = (token: string) => {
+    try {
+        const decoded = jwt.verify(token, config.jwtSecret);
+        return {
+            valid: true,
+            expired: false,
+            decoded,
+        };
+    } catch (e: any) {
+        return {
+            valid: false,
+            expired: e.message === 'jwt expired',
+            decoded: null,
+        };
+    }
+};
